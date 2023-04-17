1341a9809e2cbf070535da5dee1cd6d0b3a8afd8
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-04-17 10:30:22 -0400

    create api interface for frontend

---
+++ static/scripts/client.js
@@ -0,0 +1,51 @@
+// TODO : ajouter cette ligne en haut du fichier ejs pour importer le script
+// <script src="/static/scripts/login.js"></script>
+
+
+let clientTags;
+
+function loadElements() {
+    clientTags = {
+        getButton: $("#usernameInput"), // TODO : mettre l'id du bouton
+        // TODO : ajouter les blocs whitelist/inconnu
+    };
+}
+
+const getClients = () => {
+    return new Promise(function (resolve, reject) {
+        getClientsEndpoint = "/api/client/read";
+
+        apiCall(loginEndpoint, "GET")
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+const parseClientResponse = (response) => {
+    return new Promise(function (resolve, reject) {
+        if (response.statusCode == 401) {
+            // TODO : en cas d'erreur
+        }
+        if (response.statusCode == 200) {
+            // TODO : en cas de réussite
+        }
+        resolve();
+    });
+};
+
+$(document).ready(() => {
+    loadElements();
+
+    clientTags.getButton.click(() => {
+        getClients()
+            .then(apiResponseParser)
+            .then(parseClientResponse)
+            .catch((error) => {
+                console.log(error);
+            });
+    });
+});
