34c0d2f73b081b3a0c3c29c01a7e97722b07bee5
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-04-24 17:33:45 -0400

    make port unique

--- models/port.js
+++ models/port.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const portSchema = mongoose.Schema({
-    number: { type: Number, required: true },
+    number: { type: Number, required: true, unique: true },
     open: { type: Boolean, required: true },
 });
 
