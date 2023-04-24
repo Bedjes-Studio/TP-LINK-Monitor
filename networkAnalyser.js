3f8f40c9e9a80990bc7ffa4d621512ebbdd3fea9
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-04-24 17:34:29 -0400

    add port demon

--- networkAnalyser.js
+++ networkAnalyser.js
@@ -1,33 +1,43 @@
 const { spawn } = require("child_process");
 
+const config = require("./config");
 const mongodb = require("./mongodb");
 const Client = require("./models/client");
+const Port = require("./models/port");
 const { createClient } = require("./controllers/client");
+const { createPort } = require("./controllers/port");
 
-function startTsharkDemon() {
+function formatIPData(data) {
+    let formattedData = data.split(/\r?\n|\r|\n/g).filter((row) => row != ""); // split lines and remove empty
+    return [...new Set(formattedData)]; // remove duplicates
+}
+
+function startIpDemon() {
     const captureInterface = "-i 4";
     const fieldPrintOption = '-T fields -E "header=n"';
     const fields = "-e ip.src";
     const lineBuffered = "-l";
 
     createIPFilters().then((filter) => {
+        console.log("# Starting Ip demon");
+        console.log("Wireshark filter : " + filter);
         const child = spawn('".\\Wireshark\\tshark"', [captureInterface, filter, fieldPrintOption, fields, lineBuffered], { shell: true });
 
         child.stdout.on("data", (data) => {
             analyseIps(data.toString());
         });
 
-        // child.stderr.on("data", (data) => {
-        //     console.error(`stderr: ${data}`);
-        // });
+        child.stderr.on("data", (data) => {
+            console.error(`stderr: ${data}`);
+        });
 
-        // child.on("error", (error) => {
-        //     console.error(`error: ${error.message}`);
-        // });
+        child.on("error", (error) => {
+            console.error(`error: ${error.message}`);
+        });
 
-        // child.on("close", (code) => {
-        //     console.log(`child process exited with code ${code}`);
-        // });
+        child.on("close", (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
     });
 }
 
@@ -46,7 +56,7 @@ const createIPFilters = () => {
                     } else if (i == clients.length) {
                         filter += '"';
                     } else {
-                        filter += " || ip.src != " + clients[i].ip;
+                        filter += " && ip.src != " + clients[i].ip;
                     }
                 }
                 resolve(filter);
@@ -57,14 +67,8 @@ const createIPFilters = () => {
     });
 };
 
-function formatData(data) {
-    let formattedData = data.split(/\r?\n|\r|\n/g); // split lines
-    formattedData.pop(); // remove empty last line
-    return [...new Set(formattedData)]; // remove duplicates
-}
-
 function analyseIps(data) {
-    ips = formatData(data);
+    ips = formatIPData(data);
 
     console.log("Wireshark found " + ips.length + " ips");
     console.log(ips);
@@ -82,7 +86,8 @@ function analyseIps(data) {
                     createClient(ip, false)
                         .then(() => {
                             console.log("Client " + ip + " blacklisted");
-                            // TODO : kill child process and spawn again
+                            // TODO : kill child process and spawn again ?
+                            // create alert
                         })
                         .catch((error) => {
                             console.log("Client " + ip + " is ever blacklisted");
@@ -93,6 +98,109 @@ function analyseIps(data) {
         .catch((error) => {
             console.log(error);
         });
+}
+
+function formatPortData(data) {
+    let formattedData = data.split(/\r?\n|\r|\t|\n/g).filter((row) => row != ""); // split lines and remove empty
+    return [...new Set(formattedData)]; // remove duplicates
+}
+
+function startPortDemon() {
+    // TODO : select wifi interface
+    const captureInterface = "-i 4";
+    const fieldPrintOption = '-T fields -E "header=n"';
+    const fields = "-e tcp.dstport -e udp.dstport";
+    const lineBuffered = "-l";
+
+    createPortFilters().then((filter) => {
+        console.log("# Starting ports demon");
+        console.log("Wireshark filter : " + filter);
+        const child = spawn('".\\Wireshark\\tshark"', [captureInterface, filter, fieldPrintOption, fields, lineBuffered], {
+            shell: true,
+        });
+
+        child.stdout.on("data", (data) => {
+            analysePorts(data.toString());
+        });
+
+        child.stderr.on("data", (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        child.on("error", (error) => {
+            console.error(`error: ${error.message}`);
+        });
+
+        child.on("close", (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+    });
+}
+
+const createPortFilters = () => {
+    return new Promise(function (resolve, reject) {
+        Port.find({})
+            .then((ports) => {
+                // TODO : set passerelle ip
+                let filter = '-Y "ip.dst==' + config.router.ip;
+                if (ports.length == 0) {
+                    filter += '"';
+                    resolve(filter);
+                }
+                for (let i = 0; i <= ports.length; ++i) {
+                    if (i == ports.length) {
+                        if (ports.length > 0) {
+                            filter += ' )"';
+                        } else {
+                            filter += '"';
+                        }
+                    } else if (i == 0) {
+                        filter += " && ( ( tcp.dstport != " + ports[i].number + " || udp.dstport != " + ports[i].number + " )";
+                    } else {
+                        filter += " && ( tcp.dstport != " + ports[i].number + " || udp.dstport != " + ports[i].number + " )";
+                    }
+                }
+                console.log("long resolve");
+                resolve(filter);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+function analysePorts(data) {
+    ports = formatPortData(data);
+    console.log("Wireshark found " + ports.length + " ports");
+    console.log(ports);
+    Port.find({ number: { $in: ports } })
+        .then((knownPorts) => {
+            knownPorts = knownPorts.map((port) => port.number);
+            ports = ports.filter((port) => {
+                return !knownPorts.includes(port);
+            });
+            if (ports.length > 0) {
+                ports.forEach((port) => {
+                    createPort(port, false)
+                        .then(() => {
+                            console.log("Warning on port " + port + " created");
+                            // TODO : kill child process and spawn again ?
+                            // create alert
+                        })
+                        .catch((error) => {
+                            console.log("Warning on port " + port + " already exist");
+                        });
+                });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function startTsharkDemon() {
+    // startIpDemon();
+    startPortDemon();
 }
 
 mongodb.connect();
